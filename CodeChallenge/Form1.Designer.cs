b6da3f85cf6167d7da865b10de7eea825dcc7d0a
ThiagoPaivaAlves <88196831+ThiagoPaivaAlves@users.noreply.github.com>
2021-09-11 15:55:20 -0300

    Add project files.

---
+++ CodeChallenge/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace CodeChallenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.loadButton = new System.Windows.Forms.Button();
+            this.myGridView = new System.Windows.Forms.DataGridView();
+            this.changeChar = new System.Windows.Forms.Button();
+            this.myCombo = new System.Windows.Forms.ComboBox();
+            this.populateCombo = new System.Windows.Forms.Button();
+            this.comboShow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.myGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            resources.ApplyResources(this.loadButton, "loadButton");
+            this.loadButton.Name = "loadButton";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // myGridView
+            // 
+            resources.ApplyResources(this.myGridView, "myGridView");
+            this.myGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.myGridView.Name = "myGridView";
+            this.myGridView.RowTemplate.Height = 24;
+            // 
+            // changeChar
+            // 
+            resources.ApplyResources(this.changeChar, "changeChar");
+            this.changeChar.Name = "changeChar";
+            this.changeChar.UseVisualStyleBackColor = true;
+            this.changeChar.Click += new System.EventHandler(this.changeChar_Click);
+            // 
+            // myCombo
+            // 
+            resources.ApplyResources(this.myCombo, "myCombo");
+            this.myCombo.FormattingEnabled = true;
+            this.myCombo.Name = "myCombo";
+            this.myCombo.SelectedIndexChanged += new System.EventHandler(this.myCombo_SelectedIndexChanged);
+            // 
+            // populateCombo
+            // 
+            resources.ApplyResources(this.populateCombo, "populateCombo");
+            this.populateCombo.Name = "populateCombo";
+            this.populateCombo.UseVisualStyleBackColor = true;
+            this.populateCombo.Click += new System.EventHandler(this.populateCombo_Click);
+            // 
+            // comboShow
+            // 
+            resources.ApplyResources(this.comboShow, "comboShow");
+            this.comboShow.Name = "comboShow";
+            this.comboShow.UseVisualStyleBackColor = true;
+            this.comboShow.Click += new System.EventHandler(this.comboShow_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboShow);
+            this.Controls.Add(this.populateCombo);
+            this.Controls.Add(this.myCombo);
+            this.Controls.Add(this.changeChar);
+            this.Controls.Add(this.myGridView);
+            this.Controls.Add(this.loadButton);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.myGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.DataGridView myGridView;
+        private System.Windows.Forms.Button changeChar;
+        private System.Windows.Forms.ComboBox myCombo;
+        private System.Windows.Forms.Button populateCombo;
+        private System.Windows.Forms.Button comboShow;
+    }
+}
+
---
+++ CodeChallenge/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeChallenge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "CSV files (*.csv)|*.csv";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    PopulateGridDataFile(filePath);
+                }
+            }
+        }
+
+       
+        private void changeChar_Click(object sender, EventArgs e)
+        {
+            int rows = myGridView.Rows.Count;
+            int col = myGridView.Columns.Count;
+
+            for (int i = 0; i < rows; i++) //lines
+            {
+                for (int j = 0; j < col; j++) //columns
+                {
+                    string temp = myGridView[j,i].Value?.ToString();
+                    if(temp != null)
+                        myGridView[j, i].Value = temp.Replace('a', '@');
+                }
+            }
+
+        }
+
+        private void myCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void comboShow_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(myCombo.SelectedIndex + " - " + myCombo.SelectedItem);
+        }
+
+        private void populateCombo_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "CSV files (*.csv)|*.csv";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    PopulateComboDataFile(filePath);
+                }
+            }
+        }
+
+        private void PopulateComboDataFile(string path) 
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+            foreach (string line in lines) 
+            {
+                string[] content = line.Split(',');
+
+                Console.WriteLine(content.Length);
+                if (content.Length >= 2)
+                {
+                    int index;
+                    int.TryParse(content[0], out index);
+                    myCombo.Items.Insert(index, content[1]);
+                }
+                
+            }
+        }
+        private void PopulateGridDataFile(string path)
+        {
+            DataTable dt = new DataTable();
+            string[] lines = System.IO.File.ReadAllLines(path);
+            if (lines.Length > 0)
+            {
+                //creating headers
+                string headers = lines[0];
+                string[] labels = headers.Split(',');
+
+                foreach (string word in labels)
+                {
+                    dt.Columns.Add(new DataColumn(word));
+                }
+                //getting the data
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] lineContent = lines[i].Split(',');
+                    DataRow row = dt.NewRow(); //adding rows
+                    int index = 0;
+                    foreach (string content in labels)
+                    {
+                        row[content] = lineContent[index++];
+                    }
+                    dt.Rows.Add(row);
+                }
+
+                if (dt.Rows.Count > 0)
+                {
+                    myGridView.DataSource = dt;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
